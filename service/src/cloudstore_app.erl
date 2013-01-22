991f56f4b2f65147b24ce525aa6f915274fb635a
Peter Hizalev <peter@connect.me>
2013-01-21 17:26:10 -0800

    Erlang app.

---
+++ service/src/cloudstore_app.erl
@@ -0,0 +1,16 @@
+-module(cloudstore_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    cloudstore_sup:start_link().
+
+stop(_State) ->
+    ok.
