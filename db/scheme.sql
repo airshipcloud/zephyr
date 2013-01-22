0671bc39491f8760ce77c89da73eb4582df14992
Peter Hizalev <peter@connect.me>
2013-01-21 16:35:24 -0800

    Prototyping indexes.

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,3 +1,4 @@
-create table "objects"("id" bigint, "path" hstore, "value" hstore);
+create table "objects"("id" bigint not null, "path" hstore not null, "value" hstore not null);
 
 create index "objects_path" on "objects" using gist ("path");
+create index "objects_value" on "objects" using gist ("value");
