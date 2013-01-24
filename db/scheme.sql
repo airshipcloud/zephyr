dfcf1eaaaa4dffbd8a786b6043991135f386308a
Peter Hizalev <peter@connect.me>
2013-01-24 14:30:05 -0800

    Adjusted schema.

--- db/scheme.sql
+++ db/scheme.sql
@@ -1,4 +1,5 @@
-create table "objects"("id" bigint not null, "path" hstore not null, "value" hstore not null);
+create table "objects"("key" text not null, "version" bigint not null, "path" hstore not null, "value" hstore not null);
 
+create unique index "objects_key" on "objects" ("key");
 create index "objects_path" on "objects" using gist ("path");
 create index "objects_value" on "objects" using gist ("value");
