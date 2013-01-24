6acd2c461119f74009ae3e993beeec1efb265a41
Andrei Soroker <soroker@gmail.com>
2013-01-23 16:03:20 -0800

    Apply config script.

---
+++ apply_config.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+cd -- "${0%/*}"
+# set -x
+set -e
+
+rm -f config/applied_config
+
+args=`echo $@`
+arr=( "base" ${args} )
+
+echo "Applying ${args}"
+
+for i in ${arr[@]}
+do
+    source ./config/${i}
+    echo ${i} >> config/applied_config
+done
+
+# set; exit 0
+
+apply_config_to=( )
+
+for d in ${apply_config_to[@]}
+do
+    if [ -d ${d} ]
+    then
+        # sf -> source file
+        for sf in `find ./${d} -name *.source`
+        do
+            tf=${sf%.*} # target file
+            echo ${tf}
+            cp ${sf} ${tf}
+            > ${tf}
+IFS="
+"
+            for line in $(< ${sf}); do
+                z=`echo ${line} | sed 's_\"_\\\"_g'`
+                z=`echo ${z} | sed 's_\\\$_APPLY-CONFIG-DOLLAR'`
+                x=`echo ${z} | sed -n "s_@@\([^@]*\)@@_$\{\1\}_g p"`
+                if [ -n "${x}" ]
+                then
+                    y=`eval echo \"${x}\"`
+                    y=`echo ${y} | sed 's_APPLY-CONFIG-DOLLAR\\\$_g'`
+                    echo ${y} >> ${tf}
+                else
+                    echo ${line} >> ${tf}
+                fi
+            done # < ${sf}
+        done
+    fi
+done
