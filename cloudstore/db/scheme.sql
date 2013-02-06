6e23fc59888fd973bffbf1d3e049bd3565072b64
Peter Hizalev <peter@connect.me>
2013-02-05 16:34:48 -0800

    DB schema for tokens.

--- cloudstore/db/scheme.sql
+++ cloudstore/db/scheme.sql
@@ -3,3 +3,6 @@ create table "objects"("hash" text not null, "version" bigint not null, "path" h
 create unique index "objects_hash" on "objects" ("hash");
 create index "objects_path" on "objects" using gist ("path");
 create index "objects_value" on "objects" using gist ("value");
+
+create table "tokens"("id" text not null, "path" hstore not null, "access" text not null);
+create unique index "tokens_id_path" on "tokens" ("id", "path");
