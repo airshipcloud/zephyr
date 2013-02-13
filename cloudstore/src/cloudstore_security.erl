ada3692774992f98b8f813ba9460b22964a45a37
Peter Hizalev <peter@connect.me>
2013-02-12 20:15:02 -0800

    Client library prototype--WIP.

--- cloudstore/src/cloudstore_security.erl
+++ cloudstore/src/cloudstore_security.erl
@@ -8,7 +8,8 @@ add_cors(Req) ->
     cowboy_req:set_resp_header(<<"access-control-allow-origin">>, Origin,
     cowboy_req:set_resp_header(<<"access-control-allow-methods">>, <<"OPTIONS, GET, PUT, DELETE">>,
     cowboy_req:set_resp_header(<<"access-control-allow-credentials">>, <<"true">>,
-    cowboy_req:set_resp_header(<<"access-control-allow-headers">>, <<"origin, content-type, accept">>, Req)))).
+    cowboy_req:set_resp_header(<<"access-control-expose-headers">>, <<"etag">>,
+    cowboy_req:set_resp_header(<<"access-control-allow-headers">>, <<"origin, content-type, accept, if-match, if-not-match">>, Req))))).
 
 add_cors_options(Req) ->
     cowboy_req:set_resp_header(<<"access-control-max-age">>, <<"60">>, Req).
