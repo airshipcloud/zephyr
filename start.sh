56144af6b3ccd77f9a52ec8a306230548b8093d3
Joe Johnston <joe@connect.me>
2013-02-18 18:12:57 -0800

    Fix starting of CloudStore as daemon for Travis CI

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-./cloudstore/rel/cloudstore/bin/cloudstore console
+while getopts "d" option; do
+  case "${option}" in
+    d) DETACH=1;;
+  esac
+done
+
+if [ -z $DETACH ]; then
+  ./cloudstore/rel/cloudstore/bin/cloudstore console
+else
+  echo 'Starting CloudStore as daemon'
+  ./cloudstore/rel/cloudstore/bin/cloudstore console &
+fi
